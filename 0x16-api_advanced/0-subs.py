3a3ded1e616f188317cdc3e24d8a4d2a91a61fff
ayubsh <ibrahimayubsheikh@gmail.com>
2023-08-09 08:35:42 -0700

    404

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,12 +10,9 @@ def number_of_subscribers(subreddit):
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
             AppleWebKit/537.36 (KHTML, like Gecko)\
             Chrome/115.0.0.0 Safari/537.36"}
-
-    try:
-        res = requests.get(url, headers=headers, allow_redirects=False)
-        res.raise_for_status()
-    except res.exceptions.RequestException:
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code == 404:
         return 0
 
-    d = res.json()
-    return d['data']['subscribers']
+    d = res.json().get('data')
+    return d['subscribers']
