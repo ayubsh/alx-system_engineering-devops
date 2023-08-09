1b84dcb632a67c0669417b45eb2400bd67893956
ayubsh <ibrahimayubsheikh@gmail.com>
2023-08-09 08:42:34 -0700

    user agent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,10 +6,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
-            AppleWebKit/537.36 (KHTML, like Gecko)\
-            Chrome/115.0.0.0 Safari/537.36"}
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
     res = requests.get(url, headers=headers, allow_redirects=False)
     if res.status_code == 404:
         return 0
